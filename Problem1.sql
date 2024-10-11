267377190d61fdea59275c308f56e639ebf4bdc9
Allison <Allison.Boone@keyin.com>
2024-10-11 16:50:10 -0230

    Create student table

---
+++ Problem1.sql
@@ -0,0 +1,8 @@
+-- Student Table
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    school_enrollment_date DATE
+);
