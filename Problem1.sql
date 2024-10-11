21c5fedf230b26d0c5ed880d2dc5836a4de325ba
Allison <Allison.Boone@keyin.com>
2024-10-11 16:52:27 -0230

    Create professor table

--- Problem1.sql
+++ Problem1.sql
@@ -1,4 +1,4 @@
--- Student Table
+-- Students Table
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
@@ -6,3 +6,13 @@ CREATE TABLE students (
     email VARCHAR(50),
     school_enrollment_date DATE
 );
+
+-- Professors Table
+CREATE TABLE professors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50)
+);
+
+
