62c4b011a06b4dd6aeabe61714d2c4389b618076
Allison <Allison.Boone@keyin.com>
2024-10-11 18:29:06 -0230

    Create order_item table

--- Problem2.sql
+++ Problem2.sql
@@ -22,3 +22,14 @@ CREATE TABLE orders (
     FOREIGN KEY (customer_id) REFERENCES customers(id)
 
 );
+
+-- Order_items Table
+CREATE TABLE order_item (
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+
+);
