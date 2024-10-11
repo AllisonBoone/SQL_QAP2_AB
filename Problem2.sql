dce8411805bfeb48eed53f2587208d60d678d028
Allison <Allison.Boone@keyin.com>
2024-10-11 18:23:04 -0230

    Create customers table

--- Problem2.sql
+++ Problem2.sql
@@ -5,3 +5,11 @@ CREATE TABLE products (
     price DECIMAL(10, 2),
     stock_quantity INT
 );
+
+-- Customers Table
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name varchar(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
